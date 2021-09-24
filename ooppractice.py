d212921a427598224fa4790838f87b212b2dbd4b
Arandia <Alex.Arandia@charter.com>
2021-09-24 12:10:27 -0500

    kwargs and multi inheritance

--- ooppractice.py
+++ ooppractice.py
@@ -1,15 +1,18 @@
-from television import tvShow, anime, isekai, scifi, drama
+from television import tvShow, anime, isekai, scifi, drama, scifiAnime
 
 def main():
-    print("*"*32)
-    konosuba = isekai("KonoSuba: God's Blessing on this Wonderful World!",22,2016,"Studio Deen", "Died of Shock") # isekai subsubclass test
-    # konosuba = anime("KonoSuba: God's Blessing on this Wonderful World!",22,2016,"Studio Deen") # anime subclass test
-    # konosuba = tvShow("KonoSuba: God's Blessing on this Wonderful World!",22,2016) # tvShow class test
+    print(("*"*8)+"Subclass of a Subclass"+("*"*8))
+    konosuba = isekai(title="KonoSuba: God's Blessing on this Wonderful World!",episodeCount=22,year=2016,animationStudio="Studio Deen", arrivalMethod="Died of Shock") # isekai subsubclass test
     print(konosuba.info())
-    print("*"*32)
-    rebels = scifi("Star Wars: Rebels",75,2014,"Star Wars")
+    print(("*"*8)+"Subclass of a Subclass part 2"+("*"*8))
+    rebels = scifi(title="Star Wars: Rebels", episodeCount=75, year=2014, franchise="Star Wars")
     print(rebels.info())
-    print("*"*32)
-    breakingbad = drama("Breaking Bad",62,2008,45)
+    print(("*"*8)+"@Property decorator test"+("*"*8))
+    breakingbad = drama(title="Breaking Bad", episodeCount=62, year=2008, episodeLengthAvg=45)
+    print(breakingbad.info())
     print("The full viewing time of "+breakingbad.title+" is "+ breakingbad.fullViewingTime+".")
+    print(("*"*8)+"Multi Inheritance using super()"+("*"*8))
+    visions = scifiAnime(title="Star Wars: Visions", episodeCount=9, year=2021, franchise="Star Wars", animationStudio="Qubic Pictures")
+    print(visions.info())
+
 main()
--- television.py
+++ television.py
@@ -1,50 +1,59 @@
 import datetime
 
 class tvShow:
-    def __init__(self, title, episodeCount, year):
+    def __init__(self, title, episodeCount, year,**kwargs):
         self.title = title
         self.episodeCount = episodeCount
         self.year = year
+        assert not kwargs
     def info(self):
         return "Title: "+self.title+"\nEpisode Count: "+str(self.episodeCount)+\
             "\nYear: "+str(self.year)
 
 
 class anime(tvShow):
-    def __init__(self, title, episodeCount, year, animationStudio):
+    def __init__(self, animationStudio, **kwargs):
         self.animationStudio = animationStudio
-        super().__init__(title, episodeCount, year)
+        super().__init__(**kwargs)
     
     def info(self):
         return super().info()+"\nAnimation Studio: "+self.animationStudio
 
 
 class scifi(tvShow):
-    def __init__(self, title, episodeCount, year, franchise):
+    def __init__(self, franchise, **kwargs):
         self.franchise = franchise
-        super().__init__(title, episodeCount, year)
+        super().__init__(**kwargs)
 
     def info(self):
         return super().info()+"\nFranchise: "+self.franchise
 
 
 class isekai(anime):
-    def __init__(self, title, episodeCount, year, animationStudio, arrivalMethod):
+    def __init__(self, arrivalMethod, **kwargs):
         self.arrivalMethod = arrivalMethod
-        super().__init__(title, episodeCount, year, animationStudio)
+        super().__init__(**kwargs)
     
     def info(self):
         return super().info()+"\nIsekai Arrival Method: "+self.arrivalMethod
 
 
+class scifiAnime(scifi,anime):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+    
+    def info(self):
+        return super().info()
+
+
 class drama(tvShow):
-    def __init__(self, title, episodeCount, year, episodeLengthAvg):
+    def __init__(self, episodeLengthAvg, **kwargs):
         self.episodeLengthAvg = episodeLengthAvg
-        super().__init__(title, episodeCount, year)
+        super().__init__(**kwargs)
     
     @property
     def fullViewingTime(self):
         return str(datetime.timedelta(minutes = (self.episodeLengthAvg * self.episodeCount)))
-
-
-
+    
+    def info(self):
+        return super().info()
